c3f54702fb5c2867d8983cb69bc07faf391f1bbf
unknown <lenon.diedrich@gmail.com>
2021-01-09 14:54:41 -0300

    Add step header

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css';
 import { ReactComponent as Logo, ReactComponent} from './logo.svg';
 import React from 'react';
+import StepsHeader from './StepsHeader';
 
 function Orders() {
     return (
-        <div>
-            <h1>Orders</h1>
+        <div className="orders-container">
+            <StepsHeader />
         </div>
     )
 }
