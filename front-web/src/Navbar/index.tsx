0c24fe10676ad74db138854832b8cdb432065d7e
unknown <lenon.diedrich@gmail.com>
2021-01-09 14:38:38 -0300

    Criação das rotas

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,12 +1,13 @@
 import './styles.css';
 import { ReactComponent as Logo, ReactComponent} from './logo.svg';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="main-navbar">
             <Logo />
-            <a href="home" className="logo-text">DS Delivery</a>
+            <Link to="/" href="home" className="logo-text">DS Delivery</Link>
         </nav>
     )
 }
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+import { ReactComponent as Logo, ReactComponent} from './logo.svg';
+import React from 'react';
+
+function Orders() {
+    return (
+        <div>
+            <h1>Orders</h1>
+        </div>
+    )
+}
+
+export default Orders;
